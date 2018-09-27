ed739d7f41ce456623e05b929be0a430f88a0522
Nick <ncc314@nyu.edu>
2018-09-27 15:32:20 -0400

    mk_2

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+	private Rigidbody rb;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
