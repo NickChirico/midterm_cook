93a9c28d76d7765fddca69487bcc42d78af4ff00
Nick <ncc314@nyu.edu>
2018-10-10 21:53:13 -0400

    committing assets (mats, scripts, prefabs)

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+	// This script is attached to any "choppable" food objects the player interacts with. 
+	// It doesn't do/call anything on its own, but methods are called from the player class 
+	
+	
+	public GameObject ChoppedFood;
+	public int ChopAmount;
+
+	
+	// This "chops" the current food (gameobject) by instantiating a certain number of objects in its place and then destroying itself.
+	// This is called in the player_movement class; in the UpdateClick method
+	public void Chop()
+	{
+		//Debug.Log("CHOPPED");
+		CreateChoppedFood(ChopAmount);
+		Destroy(this.gameObject);
+	}
+
+	
+	// This just iterates through a for loop for "amount" number of times, instantiating a new object each time. 
+	// Example: if a particular ingredient should get cut into THREE pieces, this loops 3 times
+	private void CreateChoppedFood(int amount)
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			Instantiate(ChoppedFood, this.transform.position, this.transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/Item_all.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item_all : MonoBehaviour
+{
+    // This script gets attached to every holdable item
+    // It return the items name (defined in inspector) to show on the UI
+
+    // it was the simplest solution i could think of to account for a problem i was having with getting the names
+    // of all the objects that are interactable -- just attach this to every object lol and define public vars.
+    // I don't have THAT many objects in the game, so it's not that big of a pain. If i had 1000 i wouldn't do this.
+
+    public String Title;
+
+    public String GetName()
+    {
+        return Title;
+    }
+}
